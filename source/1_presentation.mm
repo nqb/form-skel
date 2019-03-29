1a52bc8d5f22719c6f2f3691229fbaf72a7b10c6
nqb <nqb+git@azyx.fr>
2019-03-29 15:41:25 +0100

    ajout mind map pour présentation

---
+++ source/1_presentation.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Pr&#xe9;sentation LOGICIEL" FOLDED="false" ID="ID_1114779739" CREATED="1553870231867" MODIFIED="1553870373792" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Nom" POSITION="right" ID="ID_496092243" CREATED="1553870233032" MODIFIED="1553870245525">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Version utilis&#xe9;e pour cette formation" POSITION="right" ID="ID_36873309" CREATED="1553870245772" MODIFIED="1553870253247">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="&#xc9;diteur / D&#xe9;veloppeur" POSITION="right" ID="ID_454130954" CREATED="1553870253944" MODIFIED="1553870262563">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Date de cr&#xe9;ation et maturit&#xe9;" POSITION="right" ID="ID_1215771064" CREATED="1553870262929" MODIFIED="1553870270885">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Philosophie" POSITION="right" ID="ID_708998913" CREATED="1553870271121" MODIFIED="1553870275626">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Langage de programmation" POSITION="right" ID="ID_329900774" CREATED="1553870276066" MODIFIED="1553870280877">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Licence" POSITION="right" ID="ID_724069523" CREATED="1553870281037" MODIFIED="1553870283252">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="Fonction" POSITION="right" ID="ID_681933491" CREATED="1553870284137" MODIFIED="1553870285757">
+<edge COLOR="#7c007c"/>
+<node TEXT="&#xc0; quoi sert ce logiciel" ID="ID_1759876150" CREATED="1553870286562" MODIFIED="1553870291139"/>
+</node>
+<node TEXT="Exemples d&apos;utilisation" POSITION="right" ID="ID_1728842362" CREATED="1553870300361" MODIFIED="1553870307080">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Mod&#xe8;le de d&#xe9;veloppement" POSITION="right" ID="ID_961213050" CREATED="1553870329832" MODIFIED="1553870335361">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="&#xc9;cosyst&#xe8;me" POSITION="right" ID="ID_1188862319" CREATED="1553870339273" MODIFIED="1553870345363">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Concurents et alternatives" POSITION="right" ID="ID_1037863912" CREATED="1553870346546" MODIFIED="1553870351651">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="En quoi il se d&#xe9;marque" POSITION="right" ID="ID_1945758315" CREATED="1553870352080" MODIFIED="1553870357517">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Chiffres" POSITION="left" ID="ID_1518740949" CREATED="1553870293850" MODIFIED="1553870295429">
+<edge COLOR="#007c7c"/>
+</node>
+</node>
+</map>
